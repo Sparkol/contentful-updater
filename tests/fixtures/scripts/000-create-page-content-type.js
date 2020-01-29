671ec8ef6a1571f9c4ebccbeb76f993edf7762d5
Jon Brace <jon.brace@sparkol.com>
2020-01-29 15:25:09 +0000

    Add update fixtures

--- tests/fixtures/scripts/000-create-page-content-type.js
+++ tests/fixtures/scripts/000-create-page-content-type.js
@@ -1,102 +1,23 @@
 module.exports = async migration => {
-  const author = migration.createContentType('page1', {
-    name: 'Page2',
-    description: 'Page creation (for multiple site)',
-    displayField: 'title',
-    fields: [
+  const page = migration.createContentType('page', {
+    name: 'Page',
+    description: 'Standard page'
+  });
+
+  page.createField('title', {
+    name: 'title',
+    type: 'Symbol',
+    localized: false,
+    required: true,
+    validations: [
       {
-        id: 'title',
-        name: 'title',
-        type: 'Symbol',
-        localized: false,
-        required: true,
-        validations: [
-          {
-            size: {
-              min: 0,
-              max: 60
-            }
-          }
-        ],
-        disabled: false,
-        omitted: false
-      },
-      {
-        id: 'slug',
-        name: 'slug',
-        type: 'Symbol',
-        localized: false,
-        required: true,
-        validations: [
-          {
-            unique: true
-          }
-        ],
-        disabled: false,
-        omitted: false
-      },
-      {
-        id: 'metaDescription',
-        name: 'meta description',
-        type: 'Symbol',
-        localized: false,
-        required: true,
-        validations: [
-          {
-            size: {
-              min: 0,
-              max: 160
-            }
-          }
-        ],
-        disabled: false,
-        omitted: false
-      },
-      {
-        id: 'metaKeywords',
-        name: 'meta keywords',
-        type: 'Symbol',
-        localized: false,
-        required: false,
-        validations: [],
-        disabled: false,
-        omitted: false
-      },
-      {
-        id: 'body',
-        name: 'body',
-        type: 'Array',
-        localized: false,
-        required: true,
-        validations: [],
-        disabled: false,
-        omitted: false,
-        items: {
-          type: 'Link',
-          validations: [
-            {
-              linkContentType: ['section']
-            }
-          ],
-          linkType: 'Entry'
+        size: {
+          min: 0,
+          max: 60
         }
-      },
-      {
-        id: 'seo',
-        name: 'seo',
-        type: 'Link',
-        localized: false,
-        required: false,
-        validations: [
-          {
-            linkContentType: ['listGridItem'],
-            message: 'use seo content model'
-          }
-        ],
-        disabled: false,
-        omitted: false,
-        linkType: 'Entry'
       }
-    ]
+    ],
+    disabled: false,
+    omitted: false
   });
 };
--- tests/fixtures/scripts/002-add-slug-field.js
+++ tests/fixtures/scripts/002-add-slug-field.js
@@ -2,8 +2,8 @@ module.exports = async migration => {
   const page = migration.editContentType('page');
 
   page
-    .createField('metaDescription')
+    .createField('slug')
     .type('Symbol')
     .required(true)
-    .name('meta description');
+    .name('url');
 };
